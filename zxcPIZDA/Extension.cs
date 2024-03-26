41c9fbe049f524a95545b52c41596a1f9f1a71a7
zxcPIZDA <newtown400@gmail.com>
2024-03-26 20:17:16 +0300

    класс расширения print

---
+++ zxcPIZDA/Extension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zxcPIZDA
+{
+    static class Extension
+    {
+public static T[] Print<T>(this T[] array)
+            {
+                foreach (var item in array)
+                    Console.Write(item + "\t");
+                Console.WriteLine();
+                return array;
+            }
+public static T Print<T>(this T item)
+            {
+                Console.WriteLine(item);
+                return item;
+            }
+}
